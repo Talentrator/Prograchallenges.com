d6ab84e21c26068b4cedaa2b44274d0a5f74b3f8
aman2712 <khanamankhan1973@gmail.com>
2021-09-14 15:50:18 +0530

    Setup frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+@font-face {
+  font-family: "Gotham Black";
+  src: url(./fonts/gotham-black-webfont.woff2);
+}
+
+@font-face {
+  font-family: "Gotham Bold";
+  src: url(./fonts/gotham-bold.woff2);
+}
+
+@font-face {
+  font-family: "Gotham Book";
+  src: url(./fonts/gotham-book.woff2);
+}
+
+#app {
+  font-family: "Gotham Book", sans-serif;
+}
+</style>
