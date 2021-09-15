0071b58bc1219e2284e4a2cf528c0dde04c77c6a
Maximilian <wittmaxi@linux.pizza>
2021-09-15 20:09:28 +0200

    [IMP:/functions] setup backend

--- frontend/src/pages/toplevel/Home.vue
+++ frontend/src/pages/toplevel/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>Hello World</h1>
+    <h1>Hello Aman</h1>
   </div>
 </template>
 
---
+++ functions/src/index.js
@@ -0,0 +1,8 @@
+import * as functions from "firebase-functions";
+
+// Start writing Firebase Functions
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello from Firebase!");
+});
