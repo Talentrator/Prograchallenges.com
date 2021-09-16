60122a02cd11e6a7cf84c3ce0aa37bb689ddacc9
Shantanu Lamba <sl.group958@gmail.com>
2021-09-16 12:35:59 +0530

    Update Banner.vue

--- frontend/src/pages/toplevel/home/Banner.vue
+++ frontend/src/pages/toplevel/home/Banner.vue
@@ -1,14 +1,9 @@
 <template>
-  <div class="bg-dark text-light">
-    <b-container class="pc-medium-container text-center py-5">
-      <h1 class="pb-5">I challenge you to challenge your friends!</h1>
-      <img
-        height="200px"
-        class="b-img b-img-fluid"
-        src="@/assets/temporary-logo.svg"
-        alt="Responsive image"
-      />
-    </b-container>
+  <div class="bannerDiv">
+      <b-container class="pc-medium-container text-center py-5">
+        <img class="responsive-image" src="@/assets/logo.jpeg" alt="Responsive Image" >
+        <h2>I challlenge you to challlenge your friends!</h2>
+      </b-container>
   </div>
 </template>
 
@@ -18,3 +13,18 @@ export default {
   name: "Banner",
 };
 </script>
+
+
+<style scoped>
+.bannerDiv {
+  background-color: black;
+  padding-top:0.25rem;
+  padding-bottom: 0.5rem;
+  color: white;
+}
+.responsive-image{
+    width: 100%;
+    max-width: 400px;
+    height: auto;
+}
+</style>
