3da7e6d9708f7e472b045a116325a9cd67f8818a
Shantanu Lamba <sl.group958@gmail.com>
2021-09-17 11:04:29 +0530

    used bootstrap instead of css

--- frontend/src/pages/toplevel/home/Banner.vue
+++ frontend/src/pages/toplevel/home/Banner.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="bannerDiv">
-      <b-container class="pc-medium-container text-center py-5">
-        <img class="responsive-image" src="@/assets/logo.jpeg" alt="Responsive Image" >
-        <h2 id="banner-tagline">I challenge you to challenge your friends!</h2>
-      </b-container>
+  <div class="bg-dark text-light">
+    <b-container class="pc-medium-container text-center py-5">
+      <img
+        height="200px"
+        class="b-img b-img-fluid"
+        src="@/assets/temporary-logo.svg"
+        alt="Responsive image"
+      />
+      <h1 class="mt-5">I challenge you to challenge your friends!</h1>
+    </b-container>
   </div>
 </template>
 
@@ -13,22 +18,3 @@ export default {
   name: "Banner",
 };
 </script>
-
-
-<style scoped>
-.bannerDiv {
-  background-color: black;
-  padding-top:0.25rem;
-  padding-bottom: 0.5rem;
-  color: white;
-}
-.responsive-image{
-    width: 100%;
-    max-width: 400px;
-    height: auto;
-}
-#banner-tagline{
-  margin-top: 1rem;
-}
-
-</style>
