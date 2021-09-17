23399c76a52801a1b455858abfd6caf0e15e692d
Maximilian <wittmaxi@linux.pizza>
2021-09-17 22:18:00 +0200

    [FIX:Footer.vue] use b-img

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="bg-dark text-light pt-4">
     <b-container>
-      <ul>
-        <li><a href="#" id="Content1" class="text-light">Home</a></li>
-        <li><a href="#" id="Content1" class="text-light">How It Works</a></li>
-        <li><a href="#" id="Content1" class="text-light">Challenges</a></li>
-        <li><a href="#" id="Content1" class="text-light">Support</a></li>
-      </ul>
-      <ul>
-        <!--Email Logo-->
-        <li id="Content">prograchallenges@gmail.com</li>
-        <!--Telegram Logo-->
-        <li id="Content">Programming Challenges</li>
-        <!--Reddit Logo-->
-        <li id="Content">r/prograchallenges</li>
-      </ul>
-      <img src="@/assets/temporary-logo.svg" />
+      <b-row>
+        <b-col>
+          <b-link class="d-block text-light">Home</b-link>
+          <b-link class="d-block text-light">How It Works</b-link>
+          <b-link class="d-block text-light">Challenges</b-link>
+          <b-link class="d-block text-light">Support</b-link>
+        </b-col>
+
+        <b-col>
+          <!--Email Logo-->
+          <b-link class="d-block text-light">prograchallenges@gmail.com</b-link>
+          <!--Telegram Logo-->
+          <b-link class="d-block text-light">Programming Challenges</b-link>
+          <!--Reddit Logo-->
+          <b-link class="d-block text-light">r/prograchallenges</b-link>
+        </b-col>
+        <b-col>
+          <!-- something about our webpack config seems broken? -->
+          <b-img :src="require('../assets/temporary-logo.svg')"></b-img>
+        </b-col>
+      </b-row>
     </b-container>
   </div>
 </template>
@@ -28,9 +34,6 @@ export default {
 </script>
 
 <style scoped>
-#Content1 {
-  text-decoration: none;
-}
 #Title {
   font-family: "Gotham Bold", sans-serif;
   font-size: xx-large;
