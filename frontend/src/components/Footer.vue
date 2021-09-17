40f36e4ba08de82b85de97404b604a9d08fb1bf4
Maximilian <wittmaxi@linux.pizza>
2021-09-17 21:32:18 +0200

    [FIX:Footer.vue] removed unecessary titles - 'show, don't tell'

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,25 +1,26 @@
 <template>
   <!--<div style="background-color:black;">-->
-    <div class="bg-dark text-light pt-4">
-      <b-container>
-        <footer class="horizontal">
-          <ul>
-              <li id="Title">Navigation</li>
-              <li><a href="#" id="Content1" class="text-light">Home</a></li>
-              <li><a href="#" id="Content1" class="text-light">How It Works</a></li>
-              <li><a href="#" id="Content1" class="text-light">Challenges</a></li>
-              <li><a href="#" id="Content1" class="text-light">Support</a></li>
-          </ul>
-          <ul>
-              <li id="Title">Contact Us</li>
-              <!--Email Logo--><li id="Content">prograchallenges@gmail.com</li>
-              <!--Telegram Logo--><li id="Content">Programming Challenges</li>
-              <!--Reddit Logo--><li id="Content">r/prograchallenges</li>
-          </ul>
-          <img src="@/assets/temporary-logo.svg" />
-        </footer>
-      </b-container>
-    </div>
+  <div class="bg-dark text-light pt-4">
+    <b-container>
+      <footer class="horizontal">
+        <ul>
+          <li><a href="#" id="Content1" class="text-light">Home</a></li>
+          <li><a href="#" id="Content1" class="text-light">How It Works</a></li>
+          <li><a href="#" id="Content1" class="text-light">Challenges</a></li>
+          <li><a href="#" id="Content1" class="text-light">Support</a></li>
+        </ul>
+        <ul>
+          <!--Email Logo-->
+          <li id="Content">prograchallenges@gmail.com</li>
+          <!--Telegram Logo-->
+          <li id="Content">Programming Challenges</li>
+          <!--Reddit Logo-->
+          <li id="Content">r/prograchallenges</li>
+        </ul>
+        <img src="@/assets/temporary-logo.svg" />
+      </footer>
+    </b-container>
+  </div>
   <!--</div>-->
 </template>
 
@@ -31,22 +32,17 @@ export default {
 </script>
 
 <style scoped>
-ul{
-    list-style: none;
-    
-}
 .horizontal {
   padding-bottom: 5rem;
   display: flex;
   justify-content: space-around;
-  
 }
-#Content1{
-    text-decoration: none;
+#Content1 {
+  text-decoration: none;
 }
-#Title{
-    font-family: 'Gotham Bold',sans-serif;
-    font-size: xx-large;
-    margin-bottom: 1rem;
+#Title {
+  font-family: "Gotham Bold", sans-serif;
+  font-size: xx-large;
+  margin-bottom: 1rem;
 }
 </style>
