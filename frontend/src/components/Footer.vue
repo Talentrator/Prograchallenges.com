41ed2a8cc169e09306589fc33d861a0af906778c
Maximilian <wittmaxi@linux.pizza>
2021-09-17 21:34:55 +0200

    [FIX:Footer.vue] removed unidiomatic/outdated html-tags

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,23 +1,21 @@
 <template>
   <div class="bg-dark text-light pt-4">
     <b-container>
-      <footer class="horizontal">
-        <ul>
-          <li><a href="#" id="Content1" class="text-light">Home</a></li>
-          <li><a href="#" id="Content1" class="text-light">How It Works</a></li>
-          <li><a href="#" id="Content1" class="text-light">Challenges</a></li>
-          <li><a href="#" id="Content1" class="text-light">Support</a></li>
-        </ul>
-        <ul>
-          <!--Email Logo-->
-          <li id="Content">prograchallenges@gmail.com</li>
-          <!--Telegram Logo-->
-          <li id="Content">Programming Challenges</li>
-          <!--Reddit Logo-->
-          <li id="Content">r/prograchallenges</li>
-        </ul>
-        <img src="@/assets/temporary-logo.svg" />
-      </footer>
+      <ul>
+        <li><a href="#" id="Content1" class="text-light">Home</a></li>
+        <li><a href="#" id="Content1" class="text-light">How It Works</a></li>
+        <li><a href="#" id="Content1" class="text-light">Challenges</a></li>
+        <li><a href="#" id="Content1" class="text-light">Support</a></li>
+      </ul>
+      <ul>
+        <!--Email Logo-->
+        <li id="Content">prograchallenges@gmail.com</li>
+        <!--Telegram Logo-->
+        <li id="Content">Programming Challenges</li>
+        <!--Reddit Logo-->
+        <li id="Content">r/prograchallenges</li>
+      </ul>
+      <img src="@/assets/temporary-logo.svg" />
     </b-container>
   </div>
 </template>
@@ -30,11 +28,6 @@ export default {
 </script>
 
 <style scoped>
-.horizontal {
-  padding-bottom: 5rem;
-  display: flex;
-  justify-content: space-around;
-}
 #Content1 {
   text-decoration: none;
 }
