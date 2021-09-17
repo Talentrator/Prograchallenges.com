2155a53e0f67410f6e1df742a7ccee8248ed827f
Shantanu Lamba <sl.group958@gmail.com>
2021-09-17 20:26:15 +0530

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <Navbar />
     <router-view />
+    <Footer />
   </div>
 </template>
 
 <script>
+import Footer from './components/Footer.vue';
 import Navbar from "./components/Navbar.vue";
 
 export default {
   name: "App",
   components: {
     Navbar,
+    Footer,
   },
 };
 </script>
