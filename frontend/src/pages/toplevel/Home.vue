6fe56e8cfcbca8fef175c21ed948aa0c5d0e606c
Shantanu Lamba <sl.group958@gmail.com>
2021-09-19 17:48:23 +0530

    [REF:Home.vue]added challenges component

--- frontend/src/pages/toplevel/Home.vue
+++ frontend/src/pages/toplevel/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <Banner />
+    <Challenges />
   </div>
 </template>
 
 <script>
 import Banner from "./home/Banner.vue";
+import Challenges from './home/Challenges.vue';
 
 export default {
   name: "Home",
   components: {
     Banner,
+    Challenges,
   },
 };
 </script>
