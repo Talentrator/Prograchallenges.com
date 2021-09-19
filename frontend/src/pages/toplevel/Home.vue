c98221b9188c6e76fcda1d7fc46fc971788b1240
Shantanu Lamba <sl.group958@gmail.com>
2021-09-19 15:36:36 +0530

    [REF:Home.vue]removed Dummy Section

--- frontend/src/pages/toplevel/Home.vue
+++ frontend/src/pages/toplevel/Home.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="home">
     <Banner />
-    <DummySection />
   </div>
 </template>
 
 <script>
 import Banner from "./home/Banner.vue";
-import DummySection from "./home/DummySection.vue";
 
 export default {
   name: "Home",
   components: {
     Banner,
-    DummySection,
   },
 };
 </script>
