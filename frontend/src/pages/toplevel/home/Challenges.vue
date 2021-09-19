ebee4d4855c5b523efef925ab4b9d29585fe89d0
Shantanu Lamba <sl.group958@gmail.com>
2021-09-19 17:46:20 +0530

    [IMP:Challenges.vue]Uploaded Challenges.vue file

---
+++ frontend/src/pages/toplevel/home/Challenges.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="bg-dark my-5">
+    <b-container class="text-light">
+      <center>
+        <h1 class="pb-2" style="font-size: 3.5rem">C h a l l e n g e s</h1>
+        <p style="letter-spacing: 1.25px">
+          Test your skills with thousands of challenges,<br />
+          easy and industry standard<br />just for you.
+        </p>
+      </center>
+      <h2 class="mt-4" style="">Browse...</h2>
+      <p style="letter-spacing: 1.4px">
+        Choose a challenge, read the problem, write code, submit , that's it!
+      </p>
+      <div>
+        <b-row>
+          <b-col><button class="button-random mt-2">RANDOM</button></b-col>
+          <b-col class="mt-3">or</b-col>
+          <b-col>
+            <b-dropdown id="dropdown-1" text="Language" class="m-md-2 mt-1">
+              <b-dropdown-item>First Action</b-dropdown-item>
+              <b-dropdown-item>Second Action</b-dropdown-item>
+              <b-dropdown-item>Third Action</b-dropdown-item>
+            </b-dropdown>
+          </b-col>
+          <b-col>
+            <b-dropdown id="dropdown-1" text="Difficulty" class="m-md-2 mt-1">
+              <b-dropdown-item>First Action</b-dropdown-item>
+              <b-dropdown-item>Second Action</b-dropdown-item>
+              <b-dropdown-item>Third Action</b-dropdown-item>
+            </b-dropdown>
+          </b-col>
+          <b-col>
+            <b-dropdown id="dropdown-1" text="Sort By" class="m-md-2 mt-1">
+              <b-dropdown-item>First Action</b-dropdown-item>
+              <b-dropdown-item>Second Action</b-dropdown-item>
+              <b-dropdown-item>Third Action</b-dropdown-item>
+            </b-dropdown>
+          </b-col>
+        </b-row>
+      </div>
+
+      <div>
+        <center>
+          <b-table  :items="items" class="text-light mt-4"></b-table>
+        </center>
+      </div>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Challenges",
+
+  data() {
+    return {
+      items: [
+        { ID:1 , Problem: "Example", Languages: "Example" ,Difficulty:"Medium"},
+        { ID:2 , Problem: "Example", Languages: "Example" ,Difficulty:"Medium"},
+        { ID:3 , Problem: "Example", Languages: "Example" ,Difficulty:"Medium"},
+        { ID:4 , Problem: "Example", Languages: "Example" ,Difficulty:"Medium"},
+        { ID:5 , Problem: "Example", Languages: "Example" ,Difficulty:"Medium"},
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+.button-random {
+  background-color: #dbb880;
+  border: none;
+  color: black;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+}
+</style>
+
