625234eef37ee44b415d4522aede4927c4125162
aman2712 <khanamankhan1973@gmail.com>
2021-09-20 11:26:08 +0530

    [REF:CreateChallenge.vue|SingleChallenge.vue] converted to dark mode

--- frontend/src/pages/create-challenge/CreateChallenge.vue
+++ frontend/src/pages/create-challenge/CreateChallenge.vue
@@ -1,7 +1,7 @@
 <template>
   <b-container>
       <div class="px-3 py-2">
-        <h1 class="text-center mt-2">Create a challenge</h1>
+        <h1 class="text-center mt-2 text-white">Create a challenge</h1>
         <p class="text-muted text-center mb-3">Create a challenge for others to face!</p>
         <b-form class="px-0 px-md-5">
           <b-form-group id="input-group-1" class="px-1 px-md-5">
