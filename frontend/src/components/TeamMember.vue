084a09e5505f1ad22381a47c80062423b04a705b
saransh-283 <saranshmm@gmail.com>
2021-09-21 23:03:51 +0530

    RVW: TeamMember.vue RVW: CoreTeam.vue

--- frontend/src/components/TeamMember.vue
+++ frontend/src/components/TeamMember.vue
@@ -1,45 +1,58 @@
 <template>
-  <div class="card border text-white">
-      <img class="card-img-top" :src="member.image" :alt="`${member.name} Picture`">
-      <div class="card-body">
-      <h4>{{member.name}}</h4>
-      <h6 class="text-muted">{{member.role}}</h6>
-      <p>{{member.bio}}</p>
-      <p><a :href="`mailto:${member.contact.email}`">{{member.contact.email}}</a></p>
-      <p><a :href="`https://t.me${member.contact.telegram}`">{{member.contact.telegram}}</a></p>
-      </div>
+  <div class="card p-0 bg-transparent border text-white">
+    <img
+      class="card-img-top"
+      :src="member.image"
+      :alt="`${member.name} Picture`"
+    />
+    <div class="card-body p-2">
+      <h4>{{ member.name }}</h4>
+      <h6 class="text-muted">{{ member.role }}</h6>
+      <p>{{ member.bio }}</p>
+      <p>
+        <a :href="`mailto:${member.contact.email}`">{{
+          member.contact.email
+        }}</a>
+      </p>
+      <p>
+        <a :href="`https://t.me/${member.contact.telegram}`">
+        @{{
+          member.contact.telegram
+        }}</a>
+      </p>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.card{
-    padding: 0;
-    background: none;
-    width: min-content;
-}
-.card-img-top{
-    width: 43vw;
-    max-width: 24rem;
-    height: 55vw;
-    max-height: 30rem;
-}
-
-.card-body{
-    padding: 2rem;
-}
-
-@media (max-width: 768px){
-    .card-img-top{
-        height: 15rem;
+    .card {
+        width: min-content;
     }
-}
+
+    .card-img-top {
+        width: 43vw;
+        max-width: 24rem;
+        height: 55vw;
+        max-height: 30rem;
+    }
+
+    @media (max-width: 768px) {
+        .card-img-top {
+            width: 80vw;
+            max-width: 30rem;
+            height: 50vw;
+            max-height: 40rem;
+        }
+    }
 </style>
 
 <script>
 export default {
-    name:'Member',
-    props:{member:{
-        type:Object
-    }}
-}
+  name: "Member",
+  props: {
+    member: {
+      type: Object,
+    },
+  },
+};
 </script>
--- frontend/src/pages/core-team/CoreTeam.vue
+++ frontend/src/pages/core-team/CoreTeam.vue
@@ -1,106 +1,109 @@
 <template>
-    <main>
-        <b-container>
-            <h1 class="text-center text-white">Meet The Team</h1>
-        <p class="text-muted text-center mb-5">Check out our lead developers</p>
-        
-            <b-row>
-        <MemberCard 
-        v-for="(member,idx) in members" 
-        :key="idx" 
-        :member="member" />
-        </b-row>
-        </b-container>
-    </main>
+  <div class="pt-3 pb-3">
+    <b-container>
+      <h1 class="text-center text-white">Meet The Team</h1>
+      <p class="text-muted text-center mb-3">Check out our lead developers</p>
+
+      <b-row class="justify-content-md-between flex-md-row align-items-center flex-column">
+        <MemberCard
+          v-for="(member, idx) in members"
+          :key="idx"
+          :member="member"
+        />
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <style scoped>
-main{
-    padding: 3rem 0;
-}
-.row{
-    justify-content: space-between;
-    row-gap: 2rem;
-    column-gap: 1rem;
-}
-@media (max-width: 768px){
-    .row{
-        flex-direction: column;
-        align-items: center;
+    .row {
+        row-gap: 2rem;
+        column-gap: 1rem;
     }
-}
 </style>
 
 <script>
-import MemberCard from '../../components/TeamMember.vue'
+import MemberCard from "../../components/TeamMember.vue";
 export default {
-    name:'CoreTeam',
-    components:{MemberCard},
-    data() {
-        return {
-            members:[
-                {
-                    name:'Member Name 1',
-                    image:'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
-                    role:'Role',
-                    bio:'Hi I am a developer working on this project lorem ipsum dolor sit amet.',
-                    contact:{
-                        email:'abc123@gmail.com',
-                        telegram:'@tg-username'
-                    }
-                },
-                {
-                    name:'Member Name 2',
-                    image:'https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                    role:'Role',
-                    bio:'Hi I am a developer working on this project lorem ipsum dolor sit amet.',
-                    contact:{
-                        email:'abc123@gmail.com',
-                        telegram:'@tg-username'
-                    }
-                },
-                {
-                    name:'Member Name 3',
-                    image:'https://images.pexels.com/photos/937481/pexels-photo-937481.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                    role:'Role',
-                    bio:'Hi I am a developer working on this project lorem ipsum dolor sit amet.',
-                    contact:{
-                        email:'abc123@gmail.com',
-                        telegram:'@tg-username'
-                    }
-                },
-                {
-                    name:'Member Name 4',
-                    image:'https://images.pexels.com/photos/2811087/pexels-photo-2811087.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                    role:'Role',
-                    bio:'Hi I am a developer working on this project lorem ipsum dolor sit amet.',
-                    contact:{
-                        email:'abc123@gmail.com',
-                        telegram:'@tg-username'
-                    }
-                },
-                {
-                    name:'Member Name 5',
-                    image:'https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                    role:'Role',
-                    bio:'Hi I am a developer working on this project lorem ipsum dolor sit amet.',
-                    contact:{
-                        email:'abc123@gmail.com',
-                        telegram:'@tg-username'
-                    }
-                },
-                {
-                    name:'Member Name 6',
-                    image:'https://images.pexels.com/photos/1898555/pexels-photo-1898555.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                    role:'Role',
-                    bio:'Hi I am a developer working on this project lorem ipsum dolor sit amet.',
-                    contact:{
-                        email:'abc123@gmail.com',
-                        telegram:'@tg-username'
-                    }
-                }
-            ]
-        }
-    },
-}
+  name: "CoreTeam",
+  components: { MemberCard },
+  data() {
+    return {
+      members: [
+        {
+          name: "Member Name 1",
+          image:
+            "https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+          role: "Role",
+          bio:
+            "Hi I am a developer working on this project lorem ipsum dolor sit amet.",
+          contact: {
+            email: "abc123@gmail.com",
+            telegram: "tg-username",
+          },
+        },
+        {
+          name: "Member Name 2",
+          image:
+            "https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+          role: "Role",
+          bio:
+            "Hi I am a developer working on this project lorem ipsum dolor sit amet.",
+          contact: {
+            email: "abc123@gmail.com",
+            telegram: "tg-username",
+          },
+        },
+        {
+          name: "Member Name 3",
+          image:
+            "https://images.pexels.com/photos/937481/pexels-photo-937481.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+          role: "Role",
+          bio:
+            "Hi I am a developer working on this project lorem ipsum dolor sit amet.",
+          contact: {
+            email: "abc123@gmail.com",
+            telegram: "tg-username",
+          },
+        },
+        {
+          name: "Member Name 4",
+          image:
+            "https://images.pexels.com/photos/2811087/pexels-photo-2811087.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+          role: "Role",
+          bio:
+            "Hi I am a developer working on this project lorem ipsum dolor sit amet.",
+          contact: {
+            email: "abc123@gmail.com",
+            telegram: "tg-username",
+          },
+        },
+        {
+          name: "Member Name 5",
+          image:
+            "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+          role: "Role",
+          bio:
+            "Hi I am a developer working on this project lorem ipsum dolor sit amet.",
+          contact: {
+            email: "abc123@gmail.com",
+            telegram: "tg-username",
+          },
+        },
+        {
+          name: "Member Name 6",
+          image:
+            "https://images.pexels.com/photos/1898555/pexels-photo-1898555.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+          role: "Role",
+          bio:
+            "Hi I am a developer working on this project lorem ipsum dolor sit amet.",
+          contact: {
+            email: "abc123@gmail.com",
+            telegram: "tg-username",
+          },
+        },
+      ],
+    };
+  },
+};
 </script>
