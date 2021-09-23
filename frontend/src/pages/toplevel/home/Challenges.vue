b1ab7185ec0623faeb22c698733029f27b4f5058
aman2712 <khanamankhan1973@gmail.com>
2021-09-23 15:20:47 +0530

    [REF:get_all_challenges.js] trying to test cors

--- frontend/src/pages/toplevel/home/Challenges.vue
+++ frontend/src/pages/toplevel/home/Challenges.vue
@@ -46,7 +46,6 @@
 </template>
 
 <script>
-import firebase from 'firebase'
 
 export default {
   name: "Challenges",
--- functions/src/challenges/get_all_challenges.js
+++ functions/src/challenges/get_all_challenges.js
@@ -1,16 +1,16 @@
-const getAllChallenges = (functions, admin) => functions.https.onCall(async () => {
-    let data = []
-  
-    await admin.firestore().collection('challenges').get().then((querySnapshot) => {
-      querySnapshot.forEach(doc => {
-        data.push({
-          id: doc.id,
-          ...doc.data(),
-        })
+const getAllChallenges = (functions, admin) => functions.https.onRequest(async (request, response) => {
+  let data = []
+
+  await admin.firestore().collection('challenges').get().then((querySnapshot) => {
+    querySnapshot.forEach(doc => {
+      data.push({
+        id: doc.id,
+        ...doc.data(),
       })
-  
-      return data
     })
+
+    response.json(data)  
+  })
 })
 
 module.exports = getAllChallenges
