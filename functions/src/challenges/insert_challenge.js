b5061ddc0619137b64d6272b9ec70e5ed3ef9ed6
aman2712 <khanamankhan1973@gmail.com>
2021-09-24 21:43:02 +0530

    [REF:insert_challenge.js] use ZOD

--- functions/src/challenges/insert_challenge.js
+++ functions/src/challenges/insert_challenge.js
@@ -1,23 +1,22 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const { z } = require('zod');
 
-const insertChallenge = functions.https.onCall(async (data) => {
-    const { text, title, email, nickname, language } = data;
-    // use ZOD for this
-    if(text === '' || email === '' || nickname === '' || title === '' || language === '')
-        throw new Error('invalid-argument', 'All fields are required');
+const insertChallenge = functions.https.onCall(async (data) => {    
+    const mySchema = z.object({
+        text: z.string().min(1),
+        title: z.string().min(1),
+        email: z.string().min(1),
+        nickname: z.string().min(1),
+    });
+
+    const InputData = mySchema.parse(data);
 
     // please make the id of the challenge-document be similiar to the title
     // so that we can have urls that look like
     // pc.com/challenge/implement-for-loop
     // A solution for this was already implemented in create_new_user.js, please, heavily reuse the code
-    await admin.firestore().collection('challenges').add({
-        title,
-        email,
-        nickname,
-        text,
-        language,
-    });
+    await admin.firestore().collection('challenges').add(InputData);
 });
 
 module.exports = insertChallenge;
