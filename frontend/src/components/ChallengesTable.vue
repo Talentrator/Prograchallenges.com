6dd56ec519f451b9e8bfcf05fa2d2af1b36b7871
saransh-283 <saranshmm@gmail.com>
2021-09-24 14:52:58 +0530

    POL and New Logo

--- frontend/src/components/ChallengesTable.vue
+++ frontend/src/components/ChallengesTable.vue
@@ -1,31 +1,56 @@
 <template>
-  <div class="bg-dark my-5">
+  <div class="bg-dark my-2">
     <b-container class="text-light">
-        <div class="text-center">
-          <b-table :current-page="currentPage" :items="challenges" :per-page="perpage" class="text-light mt-4"></b-table>
-          <b-pagination :total-rows="challenges.length" :per-page="perpage" v-model="currentPage" v-if="pagination"></b-pagination>
-        </div>
+      <div class="text-center">
+        <b-table
+          :current-page="currentPage"
+          :items="challenges"
+          :per-page="perpage"
+          class="text-light mt-4"
+          outlined
+        ></b-table>
+        <b-pagination
+          :total-rows="challenges.length"
+          :per-page="perpage"
+          v-model="currentPage"
+          v-if="pagination"
+          class="mb-0"
+          align="center"
+          limit=3
+          first-number
+          last-number
+        ></b-pagination>
+      </div>
     </b-container>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "Challenges",
-  props:{
-    challenges:{
-      type:Array
+  props: {
+    challenges: {
+      type: Array,
     },
-    pagination:{type:Boolean}
+    pagination: { type: Boolean },
   },
   data() {
     return {
-      perpage:20,
+      perpage: 20,
       currentPage: 1,
     };
   },
 };
 </script>
 
-
+<style scoped>
+.table > :not(:last-child) > :last-child > *{
+  border-bottom-color: unset !important;
+}
+.table {
+  border-color: rgba(255, 255, 255, 0.3) !important;
+}
+.page-link{
+  background-color: transparent !important;
+}
+</style>
--- frontend/src/pages/toplevel/home/Banner.vue
+++ frontend/src/pages/toplevel/home/Banner.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="bg-dark text-light">
+  <div class="bg-dark text-light border-bottom border-primary border-1">
     <b-container class="pc-medium-container text-center pt-5 pb-md-5 pb-0">
       <img
-        height="200px"
+        height="250px"
         class="b-img b-img-fluid"
         src="@/assets/logo.svg"
         alt="Responsive image"
       />
-      <h1 class="mt-5">I challenge you to challenge your friends!</h1>
+      <h1 class="mt-5 px-2">I challenge you to challenge your friends!</h1>
     </b-container>
   </div>
 </template>
