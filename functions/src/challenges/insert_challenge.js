56103912e6734830917877ef8ed0575846c40043
Maximilian Wittmer <wittmaxi@linux.pizza>
2021-09-24 09:14:32 +0200

    [REF:insert_challenge.js] use dereferencing

--- functions/src/challenges/insert_challenge.js
+++ functions/src/challenges/insert_challenge.js
@@ -2,22 +2,17 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
 const insertChallenge = functions.https.onCall(async (data) => {
-    const challengeTitle = data.title;
-    const challengeUserEmail = data.email;
-    const challengeUserNickName = data.nickname;
-    const challengeText = data.text;
-    const challengeLanguage = data.language;
-
+    const { text, title, email, nickname, text, language } = data;
     // use ZOD for this
-    if(challengeText === '' || challengeUserEmail === '' || challengeUserNickName === '' || challengeTitle === '' || challengeLanguage === '')
+    if(text === '' || email === '' || nickname === '' || title === '' || language === '')
         throw new Error('invalid-argument', 'All fields are required');
 
     await admin.firestore().collection('challenges').add({
-        title: challengeTitle,
-        email: challengeUserEmail,
-        nickname: challengeUserNickName,
-        text: challengeText,
-        language: challengeLanguage
+        title,
+        email,
+        nickname,
+        text,
+        language,
     });
 });
 
