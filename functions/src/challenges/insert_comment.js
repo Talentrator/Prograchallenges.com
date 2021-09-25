30063a07a2c34cfd2c415b6ed97d80f9ead75799
aman2712 <khanamankhan1973@gmail.com>
2021-09-25 08:12:41 +0530

    [IMP:insert_challenge.js] slug ids

--- functions/src/challenges/insert_comment.js
+++ functions/src/challenges/insert_comment.js
@@ -1,22 +1,18 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
+const { z } = require('zod');
 
 const insertComment = functions.https.onCall(async (data) => {
-    const challengeId = data.id;
-    const userEmail = data.email;
-    const userNickName = data.nickname;
-    const commentText = data.text;
+    const mySchema = z.object({
+        challengeId: z.string().min(1),
+        email: z.string().min(1),
+        nickname: z.string().min(1),
+        commentText: z.string().min(1)
+    })
 
-    // use ZOD for this
-    if (challengeId === '', userEmail === '', userNickName === '', commentText === '')
-        throw new functions.https.HttpsError('invalid-argument', 'All fields are required')
+    const InputData = mySchema.parse(data)
 
-    admin.firestore().collection('comments').add({
-        challengeId: challengeId,
-        email: userEmail,
-        nickname: userNickName,
-        text: commentText,
-    });
+    admin.firestore().collection('comments').add(InputData);
 })
 
 module.exports = insertComment;
