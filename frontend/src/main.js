e953d15a2e3986ff79a025017548784706057f3a
saransh-283 <saranshmm@gmail.com>
2021-09-30 23:27:50 +0530

    IMP: Vuelidate in CreateChallenge.vue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,8 +4,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { BootstrapVue } from "bootstrap-vue";
-import { BootstrapVueIcons } from "bootstrap-vue";
+import {
+    BootstrapVue
+} from "bootstrap-vue";
+import {
+    BootstrapVueIcons
+} from "bootstrap-vue";
+import Vuelidate from 'vuelidate'
 
 import "./firebase";
 import "bootstrap/dist/css/bootstrap.css";
@@ -16,17 +21,18 @@ import "./theme.scss";
 Vue.use(VueRouter);
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
+Vue.use(Vuelidate)
 
 Vue.config.productionTip = false;
 
 let vueInstance;
 
 firebase.auth().onAuthStateChanged(() => {
-  if (!vueInstance) {
-    vueInstance = new Vue({
-      router,
-      store,
-      render: (h) => h(App),
-    }).$mount("#app");
-  }
+    if (!vueInstance) {
+        vueInstance = new Vue({
+            router,
+            store,
+            render: (h) => h(App),
+        }).$mount("#app");
+    }
 });
--- frontend/src/pages/challenges/create-challenge/CreateChallenge.vue
+++ frontend/src/pages/challenges/create-challenge/CreateChallenge.vue
@@ -10,56 +10,124 @@
           id="input-group-1"
           class="px-1 px-md-5 w-100 mx-auto max-width"
         >
-          <b-form-input
-            name="title"
-            v-model="form.title"
-            type="text"
-            :state="validateTitle"
-            :class="{ 'mb-2': validateTitle == null || validateTitle }"
-            placeholder="Challenge Title"
-          />
-          <b-form-invalid-feedback class="mb-2" :state="validateTitle">
-            The title must be at least 6 characters long.
-          </b-form-invalid-feedback>
+          <div
+            :class="{
+              'form-group--error': $v.form.title.$error,
+              'form-group--ok': !$v.form.title.$error && $v.form.title.$dirty,
+            }"
+          >
+            <b-form-input
+              name="title"
+              v-model="form.title"
+              type="text"
+              class="form__input"
+              placeholder="Challenge Title"
+              v-model.trim="$v.form.title.$model"
+              :class="{
+                'mb-2': !$v.form.title.$error,
+              }"
+            />
+            <div v-if="$v.form.title.$dirty">
+              <div class="error mb-2" v-if="!$v.form.title.required">
+                Please input title
+              </div>
+              <div class="error mb-2" v-if="!$v.form.title.minLength">
+                Title must have at least
+                {{ $v.form.title.$params.minLength.min }} letters.
+              </div>
+            </div>
+          </div>
           <b-row>
             <b-col lg="6" md="12">
-              <b-form-input
-                name="email"
-                v-model="form.email"
-                type="email"
-                :state="validateEmail"
-                :class="{ 'mb-2': validateEmail == null || validateEmail }"
-                placeholder="codemonkey@gmail.com"
-              />
-              <b-form-invalid-feedback class="mb-2" :state="validateEmail">
-                Enter a valid email address.
-              </b-form-invalid-feedback>
+              <div
+                :class="{
+                  'form-group--error': $v.form.email.$error,
+                  'form-group--ok':
+                    !$v.form.email.$error && $v.form.email.$dirty,
+                }"
+              >
+                <b-form-input
+                  name="email"
+                  v-model="form.email"
+                  type="email"
+                  class="form__input"
+                  placeholder="codemonkey@gmail.com"
+                  v-model.trim="$v.form.email.$model"
+                  :class="{
+                    'mb-2': !$v.form.email.$error,
+                  }"
+                />
+                <div v-if="$v.form.email.$dirty">
+                  <div class="error mb-2" v-if="!$v.form.email.required">
+                    Please input email
+                  </div>
+                  <div class="error mb-2" v-if="!$v.form.email.email">
+                    Please enter a valid email
+                  </div>
+                </div>
+              </div>
             </b-col>
             <b-col lg="6" md="12">
-              <b-form-input
-                name="name"
-                v-model="form.nickname"
-                type="text"
-                :state="validateName"
-                :class="{ 'mb-2': validateName == null || validateName }"
-                placeholder="greatcoder12"
-              />
-              <b-form-invalid-feedback class="mb-2" :state="validateName">
-                Your nickname must be 5-20 characters long.
-              </b-form-invalid-feedback>
+              <div
+                :class="{
+                  'form-group--error': $v.form.nickname.$error,
+                  'form-group--ok':
+                    !$v.form.nickname.$error && $v.form.nickname.$dirty,
+                }"
+              >
+                <b-form-input
+                  name="name"
+                  v-model="form.nickname"
+                  type="text"
+                  placeholder="greatcoder12"
+                  v-model.trim="$v.form.nickname.$model"
+                  class="form__input"
+                  :class="{
+                    'mb-2': !$v.form.nickname.$error,
+                  }"
+                />
+                <div v-if="$v.form.nickname.$dirty">
+                  <div class="error mb-2" v-if="!$v.form.nickname.required">
+                    Please input a nickname
+                  </div>
+                  <div
+                    class="error mb-2"
+                    v-if="
+                      !$v.form.nickname.minLength || !$v.form.nickname.maxLength
+                    "
+                  >
+                    Your nickname must be 4-20 characters long
+                  </div>
+                </div>
+              </div>
             </b-col>
           </b-row>
-          <b-form-textarea
-            v-model="form.text"
-            rows="10"
-            name="text"
-            placeholder="Your awesome challenge here"
-            :state="validateText"
-            :class="{ 'mb-2': validateText == null || validateText }"
-          />
-          <b-form-invalid-feedback class="mb-2" :state="validateText">
-            The challenge must contain more than 60 characters.
-          </b-form-invalid-feedback>
+          <div
+            :class="{
+              'form-group--error': $v.form.text.$error,
+              'form-group--ok': !$v.form.text.$error && $v.form.text.$dirty,
+            }"
+          >
+            <b-form-textarea
+              v-model="form.text"
+              rows="10"
+              name="text"
+              placeholder="Your awesome challenge here"
+              v-model.trim="$v.form.text.$model"
+              class="form__input"
+              :class="{
+                'mb-2': !$v.form.text.$error,
+              }"
+            />
+            <div v-if="$v.form.text.$dirty">
+              <div class="error mb-2" v-if="!$v.form.text.required">
+                Please input some challenge text
+              </div>
+              <div class="error mb-2" v-if="!$v.form.text.minLength">
+                Your challenge must be at least 50 characters long
+              </div>
+            </div>
+          </div>
 
           <div class="d-md-flex my-2 justify-content-end text-primary">
             <div
@@ -87,6 +155,13 @@
 
 <script>
 import firebase from "firebase";
+import {
+  required,
+  minLength,
+  email,
+  maxLength,
+} from "vuelidate/lib/validators";
+
 export default {
   name: "CreateChallenge",
   data() {
@@ -109,13 +184,6 @@ export default {
         !this.form.text
       ) {
         alert("Please input the required fields"); // TODO: Replace this with a better way
-      } else if (
-        !this.validateTitle ||
-        !this.validateEmail ||
-        !this.validateName ||
-        !this.validateText
-      ) {
-        alert("Please enter valid information"); // TODO: Replace this with a better way
       } else {
         this.submitting = true;
         const insertChallenge = firebase
@@ -126,29 +194,26 @@ export default {
       }
     },
   },
-  computed: {
-    validateTitle() {
-      if (!this.form.title.length) return null;
-      else return this.form.title.length > 6;
-    },
-    validateEmail() {
-      if (!this.form.email.length) return null;
-      else {
-        const re =
-          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(this.form.email.toLowerCase());
-      }
-    },
-    validateName() {
-      if (!this.form.nickname.length) return null;
-      else
-        return (
-          this.form.nickname.length >= 5 && this.form.nickname.length <= 20
-        );
-    },
-    validateText() {
-      if (!this.form.text.length) return null;
-      else return this.form.text.length > 60;
+  validations: {
+    form: {
+      title: {
+        required,
+        minLength: minLength(4),
+        maxLength: maxLength(20),
+      },
+      email: {
+        required,
+        email,
+      },
+      nickname: {
+        required,
+        minLength: minLength(4),
+        maxLength: maxLength(20),
+      },
+      text: {
+        required,
+        minLength: minLength(50),
+      },
     },
   },
 };
