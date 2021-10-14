c8e1edfa1c2d0e950570a262dc07c0ca53c03707
Shantanu Lamba <sl.group958@gmail.com>
2021-10-14 16:08:22 +0530

    [REF:index.js]

--- functions/index.js
+++ functions/index.js
@@ -6,8 +6,10 @@ const getAllChallenges = require('./src/challenges/get_all_challenges');
 const getSingleChallenge = require('./src/challenges/get_single_challenge')
 const insertChallenge = require('./src/challenges/insert_challenge')
 const insertComment = require('./src/challenges/insert_comment')
+const CreateNewUser = require('./src/users/create_new_user');
 
 exports.getAllChallenges = getAllChallenges;
 exports.getSingleChallenge = getSingleChallenge;
 exports.insertChallenge = insertChallenge;
 exports.insertComment = insertComment;
+exports.CreateNewUser = CreateNewUser;
