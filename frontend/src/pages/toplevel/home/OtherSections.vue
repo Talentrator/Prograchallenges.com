652f20f7b19f876f5d9079165b493ed9aa96c7c2
Maximilian Wittmer <wittmaxi@linux.pizza>
2021-10-21 14:00:03 +0300

    [FIX:HomePage] responsiveness

--- frontend/src/pages/toplevel/home/OtherSections.vue
+++ frontend/src/pages/toplevel/home/OtherSections.vue
@@ -5,10 +5,8 @@
     </h1>
 
     <b-row class="max-width">
-      <b-col
-        class="py-2 border border-primary d-flex flex-column justify-content-between"
-      >
-        <div>
+      <b-col cols="12" md="4">
+        <div class="px-2 my-1 py-2 border border-primary">
           <h3 class="mt-2">🉐 Code And Learn</h3>
           <p class="">
             We have thousands of challenges which you can attempt and enhance
@@ -16,20 +14,16 @@
           </p>
         </div>
       </b-col>
-      <b-col
-        class="py-2 mx-2 border border-primary d-flex flex-column justify-content-between"
-      >
-        <div>
+      <b-col cols="12" md="4">
+        <div class="px-2 my-1 py-2 border border-primary">
           <h3 class="mt-2">🤗 Make Friends!</h3>
           <p class="">
             Join our vast community on Telegram and make new Coder Friends !
           </p>
         </div>
       </b-col>
-      <b-col
-        class="py-2 border border-primary d-flex flex-column justify-content-between"
-      >
-        <div>
+      <b-col cols="12" md="4">
+        <div class="px-2 my-1 py-2 border border-primary">
           <h3 class="mt-2">👷‍♂️ Create</h3>
           <p class="">
             Challenge others by creating custom challenges on our platform
@@ -39,32 +33,26 @@
     </b-row>
     <div class="border-primary border-top mt-4 border-2 pb-3 pb-sm-0">
       <h1 class="py-4">ABOUT US</h1>
-      <b-row
-        class="max-width mx-auto"
-      >
-        <b-col
-          class="py-3 justify-content-between bg-dark"
-        >
+      <b-row class="max-width mx-auto">
+        <b-col cols="12" md="6" class="px-0 py-md-3 py-1 justify-content-between bg-dark">
           <h2>The Team</h2>
           <p>
             We are programmers just like you ! Helping and inspiring each other
             to improve themselves
           </p>
 
-          <b-link :to="{ name: 'core-team' }" class="btn btn-primary mt-3">
-            View Collaborators
+          <b-link :to="{ name: 'core-team' }" class="mt-3 text-primary">
+            View Collaborators <b-icon-arrow-right-circle-fill class="ml-1" />
           </b-link>
         </b-col>
-        <b-col
-          class="px-3 py-3 bg-dark"
-        >
+        <b-col cols="12" md="6" class="px-0 px-md-3 py-md-3 py-1 bg-dark">
           <h2>The Project</h2>
           <p>
             We are all about Programming Challenges, This platform helps
             programmers enhance their skills, challenge others, make friends
           </p>
-          <b-link :to="{ name: 'challenges' }" class="btn btn-primary mt-2">
-            Explore Challenges
+          <b-link :to="{ name: 'challenges' }" class="mt-2 text-primary">
+            Explore Challenges <b-icon-arrow-right-circle-fill class="ml-1" />
           </b-link>
         </b-col>
       </b-row>
