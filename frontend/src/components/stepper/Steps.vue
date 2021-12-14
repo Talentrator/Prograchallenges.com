5178a8d9456245ee249852a51121dc2a213654dd
james-muriithi <muriithijames556@gmail.com>
2021-12-14 15:58:40 +0300

    added multistep form

---
+++ frontend/src/components/stepper/Steps.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="steps">
+    <div
+      :class="`step rounded ${index + 1 === currentStep ? 'active' : ''}`"
+      v-for="(step, index) in steps"
+      :key="step.title"
+      @click="changeStep(index + 1)"
+    >
+      <div class="d-flex align-items-center step-title">
+        {{ step.title }} ({{ index + 1 }})
+      </div>
+      <div class="step-details">
+        <p class="pt-3">
+          {{ step.description }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Steps",
+  emits: ["changeStep"],
+  props: {
+    currentStep: {
+      default: 1,
+      type: Number,
+    },
+  },
+  data() {
+    return {
+      steps: [
+        {
+          title: "Personal information",
+          description: "Let other people know who posted the challenge",
+        },
+        {
+          title: "About the challenge",
+          description:
+            "Please provide a catchy title for your challenge and a description of the challenge.",
+        },
+        {
+          title: "About the code",
+          description:
+            "Define the bolerplate code and unit tests of the challenge.",
+        },
+      ],
+    };
+  },
+  methods: {
+    changeStep(step) {
+      this.$emit("changeStep", step);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.steps {
+  margin: 20px 0 20px 20px;
+  max-width: 300px;
+  .step {
+    min-height: 50px;
+    border: 1px solid #ddd;
+    width: 100%;
+    padding-left: 10px;
+    margin-bottom: 10px;
+    cursor: pointer;
+    .step-title {
+      height: 50px;
+    }
+    &.active {
+      border: 1px solid map-get($map: $theme-colors, $key: primary);
+      .step-title {
+        font-weight: bold;
+      }
+      .step-details {
+        display: block;
+      }
+    }
+    .step-details {
+      display: none;
+      transition: all 0.3s linear;
+    }
+  }
+}
+</style>
--- frontend/src/pages/challenges/CreateChallenge.vue
+++ frontend/src/pages/challenges/CreateChallenge.vue
@@ -1,199 +1,225 @@
 <template>
   <b-container>
-    <div class="px-3 py-2 mx-auto max-width">
-      <h1 class="text-center mt-2">Create a challenge</h1>
-      <p class="text-muted text-center mb-3">
-        Write your own challenge for others to attempt!
-      </p>
-      <b-form class="px-0 gig-small-container">
-        <DropDown>
-          <template v-slot:preview=""> About yourself </template>
-          <b-row class="p-0 m-0">
-            <b-col lg="6" md="12">
-              <div
-                :class="{
-                  'form-group--error': $v.form.email.$error,
-                  'form-group--ok':
-                    !$v.form.email.$error && $v.form.email.$dirty,
-                }"
-              >
-                <b-form-input
-                  name="email"
-                  v-model="form.email"
-                  type="email"
-                  class="form__input"
-                  placeholder="codemonkey@gmail.com"
-                  v-model.trim="$v.form.email.$model"
-                  :class="{
-                    'mb-2': !$v.form.email.$error,
-                  }"
-                />
-                <div v-if="$v.form.email.$dirty">
-                  <div class="error mb-2" v-if="!$v.form.email.required">
-                    Please input email
-                  </div>
-                  <div class="error mb-2" v-if="!$v.form.email.email">
-                    Please enter a valid email
-                  </div>
-                </div>
-              </div>
-            </b-col>
-            <b-col lg="6" md="12">
-              <div
-                :class="{
-                  'form-group--error': $v.form.nickname.$error,
-                  'form-group--ok':
-                    !$v.form.nickname.$error && $v.form.nickname.$dirty,
-                }"
-              >
-                <b-form-input
-                  name="name"
-                  v-model="form.nickname"
-                  type="text"
-                  placeholder="greatcoder12"
-                  v-model.trim="$v.form.nickname.$model"
-                  class="form__input"
-                  :class="{
-                    'mb-2': !$v.form.nickname.$error,
-                  }"
-                />
-                <div v-if="$v.form.nickname.$dirty">
-                  <div class="error mb-2" v-if="!$v.form.nickname.required">
-                    Please input a nickname
-                  </div>
+    <b-row>
+      <b-col md="4" class="d-flex align-items-center">
+        <steps :currentStep="currentStep" @changeStep="changeStep" />
+      </b-col>
+      <b-col md="8">
+        <div class="px-3 py-2 mx-auto max-width">
+          <h1 class="text-center mt-2">Create a challenge</h1>
+          <p class="text-muted text-center mb-3">
+            Write your own challenge for others to attempt!
+          </p>
+          <b-form class="px-0 gig-small-container">
+            <template v-if="currentStep == 1">
+              <h3>About yourself</h3>
+              <b-row class="p-0 m-0">
+                <b-col lg="12" md="12">
                   <div
-                    class="error mb-2"
-                    v-if="
-                      !$v.form.nickname.minLength || !$v.form.nickname.maxLength
-                    "
+                    :class="{
+                      'form-group--error': $v.form.email.$error,
+                      'form-group--ok':
+                        !$v.form.email.$error && $v.form.email.$dirty,
+                    }"
                   >
-                    Your nickname must be 4-20 characters long
+                    <label class="form-label">Email</label>
+                    <b-form-input
+                      name="email"
+                      v-model="form.email"
+                      type="email"
+                      class="form__input"
+                      placeholder="codemonkey@gmail.com"
+                      v-model.trim="$v.form.email.$model"
+                      :class="{
+                        'mb-2': !$v.form.email.$error,
+                      }"
+                    />
+                    <div v-if="$v.form.email.$dirty">
+                      <div class="error mb-2" v-if="!$v.form.email.required">
+                        Please input email
+                      </div>
+                      <div class="error mb-2" v-if="!$v.form.email.email">
+                        Please enter a valid email
+                      </div>
+                    </div>
+                  </div>
+                </b-col>
+                <b-col lg="12" md="12">
+                  <div
+                    :class="{
+                      'form-group--error': $v.form.nickname.$error,
+                      'form-group--ok':
+                        !$v.form.nickname.$error && $v.form.nickname.$dirty,
+                    }"
+                  >
+                    <label class="form-label">Username</label>
+                    <b-form-input
+                      name="name"
+                      v-model="form.nickname"
+                      type="text"
+                      placeholder="greatcoder12"
+                      v-model.trim="$v.form.nickname.$model"
+                      class="form__input"
+                      :class="{
+                        'mb-2': !$v.form.nickname.$error,
+                      }"
+                    />
+                    <div v-if="$v.form.nickname.$dirty">
+                      <div class="error mb-2" v-if="!$v.form.nickname.required">
+                        Please input a nickname
+                      </div>
+                      <div
+                        class="error mb-2"
+                        v-if="
+                          !$v.form.nickname.minLength ||
+                          !$v.form.nickname.maxLength
+                        "
+                      >
+                        Your nickname must be 4-20 characters long
+                      </div>
+                    </div>
+                  </div>
+                </b-col>
+                <div class="text-center mt-2">
+                  <b-button variant="outline-primary" @click="changeStep(2)"> Next </b-button>
+                </div>
+              </b-row>
+            </template>
+
+            <template v-if="currentStep == 2">
+              <h3>About the challenge</h3>
+
+              <form
+                :class="{
+                  'form-group--error': $v.form.title.$error,
+                  'form-group--ok':
+                    !$v.form.title.$error && $v.form.title.$dirty,
+                }"
+              >
+                <b-form-input
+                  name="title"
+                  v-model="form.title"
+                  type="text"
+                  class="form__input"
+                  placeholder="Challenge Title"
+                  v-model.trim="$v.form.title.$model"
+                  :class="{
+                    'mb-2': !$v.form.title.$error,
+                  }"
+                />
+                <div v-if="$v.form.title.$dirty">
+                  <div class="error mb-2" v-if="!$v.form.title.required">
+                    Please input title
+                  </div>
+                  <div class="error mb-2" v-if="!$v.form.title.minLength">
+                    Title must have at least
+                    {{ $v.form.title.$params.minLength.min }} letters.
                   </div>
                 </div>
-              </div>
-            </b-col>
-          </b-row>
-        </DropDown>
-        <DropDown>
-          <template v-slot:preview=""> About the challenge</template>
+              </form>
 
-          <form
-            :class="{
-              'form-group--error': $v.form.title.$error,
-              'form-group--ok': !$v.form.title.$error && $v.form.title.$dirty,
-            }"
-          >
-            <b-form-input
-              name="title"
-              v-model="form.title"
-              type="text"
-              class="form__input"
-              placeholder="Challenge Title"
-              v-model.trim="$v.form.title.$model"
-              :class="{
-                'mb-2': !$v.form.title.$error,
-              }"
-            />
-            <div v-if="$v.form.title.$dirty">
-              <div class="error mb-2" v-if="!$v.form.title.required">
-                Please input title
-              </div>
-              <div class="error mb-2" v-if="!$v.form.title.minLength">
-                Title must have at least
-                {{ $v.form.title.$params.minLength.min }} letters.
-              </div>
-            </div>
-          </form>
-
-          <form
-            :class="{
-              'form-group--error': $v.form.text.$error,
-              'form-group--ok': !$v.form.text.$error && $v.form.text.$dirty,
-            }"
-          >
-          <b-button variant="secondary" size="sm" class="px-1 py-1 mb-1 text-capitalize" @click="previewMarkdown = !previewMarkdown"
-            >{{previewMarkdown ? "Continue Editing": "Preview"}}
-            </b-button
-          >
-            <div v-if="!previewMarkdown">
-              <b-form-textarea
-                v-model="form.text"
-                rows="10"
-                name="text"
-                placeholder="Your awesome challenge here"
-                v-model.trim="$v.form.text.$model"
-                class="form__input"
+              <form
                 :class="{
-                  'mb-2': !$v.form.text.$error,
+                  'form-group--error': $v.form.text.$error,
+                  'form-group--ok': !$v.form.text.$error && $v.form.text.$dirty,
                 }"
-              />
-              <div v-if="$v.form.text.$dirty">
-                <div class="error mb-2" v-if="!$v.form.text.required">
-                  Please input some challenge text
+              >
+                <b-button
+                  variant="secondary"
+                  size="sm"
+                  class="px-1 py-1 mb-1 text-capitalize"
+                  @click="previewMarkdown = !previewMarkdown"
+                  >{{ previewMarkdown ? "Continue Editing" : "Preview" }}
+                </b-button>
+                <div v-if="!previewMarkdown">
+                  <b-form-textarea
+                    v-model="form.text"
+                    rows="10"
+                    name="text"
+                    placeholder="Your awesome challenge here"
+                    v-model.trim="$v.form.text.$model"
+                    class="form__input"
+                    :class="{
+                      'mb-2': !$v.form.text.$error,
+                    }"
+                  />
+                  <div v-if="$v.form.text.$dirty">
+                    <div class="error mb-2" v-if="!$v.form.text.required">
+                      Please input some challenge text
+                    </div>
+                    <div class="error mb-2" v-if="!$v.form.text.minLength">
+                      Your challenge description must be at least 20 characters
+                      long
+                    </div>
+                  </div>
                 </div>
-                <div class="error mb-2" v-if="!$v.form.text.minLength">
-                  Your challenge description must be at least 20 characters long
+                <div v-else>
+                  <div v-html="markdownToHtml"></div>
                 </div>
+                <b-form-group
+                  class="pt-2"
+                  label="Test Programming language"
+                  label-class="font-weight-bold"
+                >
+                  <b-form-select
+                    v-model="form.programmingLanguage"
+                    :options="languages"
+                    class="w-100 form-select"
+                  />
+                </b-form-group>
+              </form>
+
+              <div class="text-center mt-2">
+                <b-button variant="outline-primary" @click="changeStep(3)"> Next </b-button>
               </div>
-            </div>
-            <div v-else>
-              <div v-html="markdownToHtml"></div>
-            </div>
-            <b-form-group
-              class="pt-2"
-              label="Test Programming language"
-              label-class="font-weight-bold"
-            >
-              <b-form-select
-                v-model="form.programmingLanguage"
-                :options="languages"
+            </template>
+
+            <template v-if="currentStep == 3">
+              <h3>About the code</h3>
+              Boilerplate Code
+              <CodeEditor
+                v-model="form.boilerplate"
+                :lang="form.programmingLanguage"
+                :key="form.programmingLanguage + '1'"
+                editorHeight="200px"
               />
-            </b-form-group>
-          </form>
-        </DropDown>
-        <DropDown>
-          <template v-slot:preview=""> About the code </template>
-          Boilerplate Code
-          <CodeEditor
-            v-model="form.boilerplate"
-            :lang="form.programmingLanguage"
-            :key="form.programmingLanguage + '1'"
-            editorHeight="200px"
-          />
-          Unit-Test for the code.
-          <CodeEditor
-            v-model="form.unitTest"
-            :lang="form.programmingLanguage"
-            :key="form.programmingLanguage + '2'"
-            editorHeight="200px"
-          />
-          Example solution to check that your unit tests are correct.
-          <CodeEditor
-            v-model="form.exampleSolution"
-            :lang="form.programmingLanguage"
-            :key="form.programmingLanguage + '3'"
-            editorHeight="200px"
-          />
-          <div class="text-center">
-            <b-button block @click="runTest()" class="mt-2 p-1">
-              <b-icon-caret-right v-if="!runningCode" />
-              <b-spinner small v-else />
-            </b-button>
-          </div>
-          <div v-if="codeOutput" class="bg-light text-dark mt-2 px-2">
-            {{ codeOutput }}
-          </div>
-        </DropDown>
-        {{ form.programmingLanguage }}
-        <div class="d-md-flex my-2 justify-content-end text-primary">
-          <b-button variant="outline-primary" @click="handleSubmit">
-            SUBMIT
-            <b-spinner variant="primary" small v-if="submitting" />&nbsp;
-            <b-icon-arrow-right-circle-fill v-if="!submitting" />
-          </b-button>
+              Unit-Test for the code.
+              <CodeEditor
+                v-model="form.unitTest"
+                :lang="form.programmingLanguage"
+                :key="form.programmingLanguage + '2'"
+                editorHeight="200px"
+              />
+              Example solution to check that your unit tests are correct.
+              <CodeEditor
+                v-model="form.exampleSolution"
+                :lang="form.programmingLanguage"
+                :key="form.programmingLanguage + '3'"
+                editorHeight="200px"
+              />
+              <div class="text-center">
+                <b-button block @click="runTest()" class="mt-2 p-1">
+                  <b-icon-caret-right v-if="!runningCode" />
+                  <b-spinner small v-else />
+                </b-button>
+              </div>
+              <div v-if="codeOutput" class="bg-light text-dark mt-2 px-2">
+                {{ codeOutput }}
+              </div>
+
+              {{ form.programmingLanguage }}
+              <div class="d-md-flex my-2 justify-content-end text-primary">
+                <b-button variant="outline-primary" @click="handleSubmit">
+                  SUBMIT
+                  <b-spinner variant="primary" small v-if="submitting" />&nbsp;
+                  <b-icon-arrow-right-circle-fill v-if="!submitting" />
+                </b-button>
+              </div>
+            </template>
+          </b-form>
         </div>
-      </b-form>
-    </div>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
@@ -207,13 +233,14 @@ import {
   maxLength,
 } from "vuelidate/lib/validators";
 import CodeEditor from "@/components/code-editor/CodeEditor.vue";
-import DropDown from "@/components/DropDown.vue";
-import {marked} from "marked";
+import { marked } from "marked";
+import Steps from "@/components/stepper/Steps.vue";
 
 export default {
   name: "CreateChallenge",
   data() {
     return {
+      currentStep: 1,
       form: {
         title: "",
         email: "",
@@ -249,6 +276,24 @@ export default {
     },
   },
   methods: {
+    changeStep(step) {
+      if (step == 2 && this.isValidStepOne()) {
+        this.currentStep = step;
+      } else if(step == 3 && this.isValidStepTwo()){
+        this.currentStep = step;
+      }
+    },
+    isValidStepOne() {
+      this.$v.form.email.$touch();
+      this.$v.form.nickname.$touch();
+      return !this.$v.form.email.$error && !this.$v.form.nickname.$error;
+    },
+    isValidStepTwo() {
+      this.$v.form.title.$touch();
+      this.$v.form.text.$touch();
+
+      return !this.$v.form.title.$error && !this.$v.form.text.$error;
+    },
     async handleSubmit() {
       this.$v.$touch();
       if (!this.$v.$invalid) {
@@ -297,7 +342,7 @@ ${this.form.unitTest}`,
   },
   components: {
     CodeEditor,
-    DropDown,
+    Steps,
   },
 };
 </script>
