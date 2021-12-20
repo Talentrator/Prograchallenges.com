6d5109b1cf5f281c1ff2e7617217f7c3873ffffe
james-muriithi <muriithijames556@gmail.com>
2021-12-20 21:58:21 +0300

    added challenge score

--- functions/src/challenges/get_all_challenges.js
+++ functions/src/challenges/get_all_challenges.js
@@ -1,19 +1,19 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 
-const getAllChallenges = functions.https.onCall(async () => {
-  let data = [];
+const getAllChallenges = functions.https.onCall(async() => {
+    let data = [];
 
-  const result = await admin.firestore().collection('challenges').orderBy("creationTime", "desc").get();
+    const result = await admin.firestore().collection('challenges').orderBy("creationTime", "desc").get();
 
-  result.forEach(doc => {
-    data.push({
-      id: doc.id,
-      ...doc.data(),
-    })
-  });
+    result.forEach(doc => {
+        data.push({
+            id: doc.id,
+            ...doc.data(),
+        })
+    });
 
-  return data;
+    return data;
 })
 
 module.exports = getAllChallenges;
