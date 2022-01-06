a1675f8e34beca9fd8c095567941a71c1149208f
james-muriithi <muriithijames556@gmail.com>
2022-01-06 16:58:31 +0300

    challenge unit tests results

--- frontend/src/components/code-editor/ChallengeModule.vue
+++ frontend/src/components/code-editor/ChallengeModule.vue
@@ -22,7 +22,17 @@
             <div class="mt-2 mx-2 px-2 py-2 text-light">
               <div v-if="compiling"><b>Status: </b>Sending request...</div>
               <div v-else-if="result">
-                <span style="white-space: pre-wrap;">
+                <div class="" v-if="!erronoeusResult">
+                  <div class="text-secondary d-flex align-items-center">
+                    <b-icon-check2-circle class="text-secondary fs-2" />
+                    <span class="ui-icon-label tab-item-label ps-1"> {{passedTestsFormattedText}} </span>
+                  </div>
+                  <div class="mt-2 text-primary d-flex align-items-center" v-if="failed > 0">
+                    <b-icon-x class="text-primary fs-2" />
+                    <span class="ui-icon-label tab-item-label ps-1"> {{failed}} test{{ passed > 1 ? 's' : '' }} failed </span>
+                  </div>
+                </div>
+                <span style="white-space: pre-wrap;" v-else>
                   {{ result }}
                 </span>
               </div>
@@ -75,11 +85,20 @@ export default {
     activeTab: 0,
     result: "",
     compiling: false,
+    passed: 0,
+    failed: 0,
   }),
   computed: {
     ...mapState(["userObject"]),
     compiledMarkdown() {
       return marked(this.challenge.text);
+    },
+    passedTestsFormattedText(){
+      const count = (this.failed == 0 && this.passed > 0) ? 'All': this.passed;
+      return `${count} test${ this.passed > 1 ? 's' : '' } passed`;
+    },
+    erronoeusResult(){
+      return !(this.result.indexOf('true') >= 0 || this.result.indexOf('false') >= 0);
     }
   },
   props: {
@@ -113,6 +132,7 @@ export default {
     async submit() {
       this.activeTab = 1;
       this.compiling = true;
+      this.failed = this.passed = 0;
 
       const response = await firebase.functions().httpsCallable("EvaluateChallenge")({
         challengeId: this.$route.params.id,
@@ -122,6 +142,7 @@ export default {
 
       this.result = response.data;
       this.compiling = false;
+      this.parseResult();
     },
     getLanguageId() {
       const mapping = {
@@ -130,6 +151,11 @@ export default {
       };
       return mapping[this.challenge.programmingLanguage];
     },
+    parseResult(){
+      if (!this.erronoeusResult) {
+        this.result.split(/\s+/).map((res) => res === 'true' ? this.passed++ : this.failed++);
+      }
+    }
   },
 };
 </script>
