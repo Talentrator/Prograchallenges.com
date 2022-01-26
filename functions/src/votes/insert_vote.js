edf37101fedbb56152deb5ab5d5349944bd4318d
james-muriithi <muriithijames556@gmail.com>
2022-01-26 17:24:20 +0300

    updated voting functionality

--- functions/src/votes/insert_vote.js
+++ functions/src/votes/insert_vote.js
@@ -1,6 +1,6 @@
 const functions = require('firebase-functions')
-const admin = require('firebase-admin')
 const { z } = require('zod');
+const { userAlreadyVoted, insertVote } = require("../tools/voting_functions")
 const { generateError } = require("../tools/error_templates");
 
 const voteSchema = z.object({
@@ -9,25 +9,6 @@ const voteSchema = z.object({
     userId: z.string()
 });
 
-const collection = "votes";
-
-async function userAlreadyVoted(documentId, userId) {
-    let votesEntity = await (await getVotesEntityRef(documentId)).get();
-
-    return votesEntity.exists && votesEntity.data().votes.some(vote => vote.votedBy == userId);
-}
-
-async function getVotesEntityRef(documentId) {
-    return await admin.firestore().collection(collection).doc(documentId);
-}
-
-async function insertVote({ entityId, votes, userId }) {
-    const votesEntity = await getVotesEntityRef(entityId);
-    votesEntity.set({
-        total_votes: admin.firestore.FieldValue.increment(+votes),
-        votes: [{ votedBy: userId, votes }]
-    }, { merge: true });
-}
 
 /**
  * Inserts votes to the votes collection
