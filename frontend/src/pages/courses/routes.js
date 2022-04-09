00176b9f28ce50792ba35ad59742275b4f601280
Shantanu Lamba <sl.group958@gmail.com>
2022-04-09 15:46:15 +0700

    [REF:courses/mainpage.vue]

--- frontend/src/pages/courses/routes.js
+++ frontend/src/pages/courses/routes.js
@@ -1,8 +1,17 @@
 export default [
     {
-      path: "/challenge/:id",
+      path: "/courses",
       name: "courses",
       component: () => import("./mainpage.vue"),
+      props: true,
+      meta: {
+        title: "Courses - Prograchallenges"
+      },
+    },
+    {
+      path: "/ethercourse",
+      name: "ethereum",
+      component: () => import("./ethercourse.vue"),
       props: true,
       meta: {
         title: "Courses - Prograchallenges"
